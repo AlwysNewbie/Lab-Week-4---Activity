6ea1baf153f128d990c5d8560c6b595067c87164
AlwysNewbie <yun.ye-1@student.uts.edu.au>
2023-09-02 16:56:05 +1000

    last

--- Assets/LoadAssets.cs
+++ Assets/LoadAssets.cs
@@ -8,7 +8,9 @@ public class LoadAssets : MonoBehaviour
     [SerializeField]
     private GameObject blueObj; // Private game object member variable, but visible in inspector view
 
-    private int i;
+    private int i;  // Make i a member variable of the class
+    private GameObject redInstance;  // Reference to the instantiated redObj
+    private GameObject blueInstance; // Reference to the instantiated blueObj
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +18,11 @@ public class LoadAssets : MonoBehaviour
         // Limit the framerate to 60
         Application.targetFrameRate = 60;
 
-        // Create a redObj at (2,0,0) with a rotation of zero
-        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
+        // Create a redObj at (2,0,0) with a rotation of zero and store its reference
+        redInstance = Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
 
-        // Create a blueObj at (-2,0,0) with a rotation of zero
-        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
+        // Create a blueObj at (-2,0,0) with a rotation of zero and store its reference
+        blueInstance = Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
 
         i = 3;
     }
@@ -31,7 +33,8 @@ public class LoadAssets : MonoBehaviour
         // Increment i by 1 at the start of every frame
         i++;
 
-        // Print the desired message to the console
-        Debug.Log(gameObject.name + ":" + i);
+        // Print the desired message to the console for both red and blue instances
+        Debug.Log(redInstance.name + ":" + i);
+        Debug.Log(blueInstance.name + ":" + i);
     }
 }
--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class PrintAndHide : MonoBehaviour
 {
     public Renderer rend;  // Public member variable
+    private int i;
+    private int randomValue;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        i = 0; // Initialize i to 0
+        randomValue = Random.Range(150, 251); // Generates a random integer between 150 and 250 inclusive
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Increment i by 1 at the start of every frame
+        i++;
+
+        // Check for the Red tag condition
+        if (gameObject.CompareTag("Red") && i == 100)
+        {
+            gameObject.SetActive(false); // Deactivate the game object
+        }
+
+        // Check for the Blue tag condition
+        if (gameObject.CompareTag("Blue") && i == randomValue)
+        {
+            if (rend != null)
+            {
+                rend.enabled = false; // Disable the Renderer component
+            }
+        }
     }
 }
